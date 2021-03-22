39d2e431c1e28b849d2daaa0a1ff1b9382d5c259
Shengyou Fan <shengyoufan@gmail.com>
2021-03-22 18:18:13 +0800

    add SQLite dependencies

--- laravel/build.gradle.kts
+++ laravel/build.gradle.kts
@@ -4,6 +4,8 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val exposed_version: String by project
+val sqlite_version: String by project
 
 plugins {
     application
@@ -28,5 +30,9 @@ dependencies {
     implementation("io.ktor:ktor-serialization:$ktor_version")
     implementation("io.ktor:ktor-server-netty:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("org.jetbrains.exposed:exposed-core:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposed_version")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposed_version")
+    implementation("org.xerial:sqlite-jdbc:$sqlite_version")
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
 }
