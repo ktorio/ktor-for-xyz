9647b604acebc7b6f326b80952cbbe88ab15c86b
Shengyou Fan <shengyoufan@gmail.com>
2021-03-22 18:12:24 +0800

    laravel sample code init

---
+++ laravel/src/main/kotlin/io/kraftsman/Application.kt
@@ -0,0 +1,37 @@
+package io.kraftsman
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.features.*
+import io.ktor.serialization.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+
+fun main(args: Array<String>): Unit =
+    io.ktor.server.netty.EngineMain.main(args)
+
+/**
+ * Please note that you can use any other name instead of *module*.
+ * Also note that you can have more then one modules in your application.
+ * */
+@Suppress("unused") // Referenced in application.conf
+@kotlin.jvm.JvmOverloads
+fun Application.module(testing: Boolean = false) {
+
+    install(ContentNegotiation) {
+        json()
+    }
+
+    routing {
+        get("/") {
+            call.respondText("Hello World!")
+        }
+    }
+    routing {
+        get("/json/kotlinx-serialization") {
+            call.respond(mapOf("hello" to "world"))
+        }
+    }
+}
+
---
+++ laravel/src/test/kotlin/io/kraftsman/ApplicationTest.kt
@@ -0,0 +1,23 @@
+package io.kraftsman
+
+import io.ktor.routing.*
+import io.ktor.http.*
+import io.ktor.features.*
+import io.ktor.serialization.*
+import io.ktor.application.*
+import io.ktor.response.*
+import io.ktor.request.*
+import kotlin.test.*
+import io.ktor.server.testing.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() {
+        withTestApplication({ module(testing = true) }) {
+            handleRequest(HttpMethod.Get, "/").apply {
+                assertEquals(HttpStatusCode.OK, response.status())
+                assertEquals("Hello World!", response.content)
+            }
+        }
+    }
+}
